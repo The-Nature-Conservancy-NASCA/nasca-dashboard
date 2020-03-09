c9fb9641e69e7cbc884096443be7624988727d7a
Marcelo Villa <marcelo.villa.p@gmail.com>
2020-03-09 10:42:11 -0500

    centrado vertical de labels y mejora de interactividad

--- src/components/boxContents/Treemap.vue
+++ src/components/boxContents/Treemap.vue
@@ -84,7 +84,7 @@ export default {
       d3
         .treemap()
         .size([this.width, this.height])
-        .padding(0)
+        .padding(1)
         .round(true)(root);
       try {
         const that = this;
@@ -97,11 +97,19 @@ export default {
             d.parent.data.name.toLowerCase().replace(/[^A-Z0-9]/gi, "_")
           )
           .on("mouseover", function(d) {
-            treemapGroup.selectAll("rect").attr("fill-opacity", 0.2);
+            treemapGroup
+              .selectAll("rect")
+              .attr("fill-opacity", 0.2)
+              .attr("stroke-opacity", 0.3);
+            treemapGroup.selectAll("text").attr("fill-opacity", 0.3);
+            treemapGroup
+              .select(`text#${that.cleanString(d.parent.data.name)}`)
+              .attr("fill-opacity", 1)
+              .attr("font-weight", "bold");
             d3.select(this)
-              .attr("stroke", "black")
               .attr("stroke-width", 1)
-              .attr("fill-opacity", 0.85);
+              .attr("fill-opacity", 1)
+              .attr("stroke-opacity", 1);
             const coordinates = [d3.event.pageX, d3.event.pageY];
             const value = Number(Math.round(d.data.value)).toLocaleString("en");
             const tooltipContent = `
@@ -128,20 +136,24 @@ export default {
           .on("mouseout", function() {
             treemapGroup
               .selectAll("rect")
-              .attr("fill-opacity", 0.8)
-              .attr("stroke-width", 0.25)
-              .attr("stroke", "gray");
+              .attr("fill-opacity", 0.9)
+              .attr("stroke-opacity", 1)
+              .attr("stroke-width", 0.5);
+            treemapGroup
+              .selectAll("text")
+              .attr("fill-opacity", 1)
+              .attr("font-weight", "normal");
             d3.select("#tooltip__graph")
               .html("")
               .style("display", "none");
           })
           .attr("x", d => d.x0)
           .attr("y", d => d.y0)
-          .attr("fill-opacity", 0.8)
           .attr("fill", d => d.data.color)
-          // .attr("fill", d => this.color(d.parent.data.name))
-          .attr("stroke-width", 0.25)
-          .attr("stroke", "gray")
+          .attr("fill-opacity", 0.9)
+          .attr("stroke", d => this.shadeColor(d.data.color, -20))
+          .attr("stroke-width", 0.5)
+          .attr("stroke-opacity", 1)
           .attr("width", d => d.x1 - d.x0)
           .attr("height", d => d.y1 - d.y0);
         treemapGroup
@@ -149,11 +161,14 @@ export default {
           .data(this.graphData.children)
           .enter()
           .append("text")
+          .attr("id", d => this.cleanString(d.name))
           .attr("class", "parent__label")
           .attr("text-anchor", "middle")
           .attr("dominant-baseline", "middle")
-          .attr("font-size", 9)
-          .attr("fill", "white")
+          .attr("pointer-events", "none")
+          .attr("font-size", 10)
+          .attr("fill", "black")
+          .attr("fill-opacity", 1)
           .attr("x", d => {
             const center = that.computeElementsCenter(
               d3
@@ -201,6 +216,9 @@ export default {
         unselected: this.selectedYear !== year
       };
     },
+    cleanString(string) {
+      return string.toLowerCase().replace(/[^A-Z0-9]/gi, "_");
+    },
     computeElementsBBox(selection) {
       const coordinates = {
         xmin: [],
@@ -227,6 +245,28 @@ export default {
       const x = xmin + (xmax - xmin) / 2;
       const y = ymin + (ymax - ymin) / 2;
       return { x, y };
+    },
+    shadeColor(color, percent) {
+      var R = parseInt(color.substring(1, 3), 16);
+      var G = parseInt(color.substring(3, 5), 16);
+      var B = parseInt(color.substring(5, 7), 16);
+
+      R = parseInt((R * (100 + percent)) / 100);
+      G = parseInt((G * (100 + percent)) / 100);
+      B = parseInt((B * (100 + percent)) / 100);
+
+      R = R < 255 ? R : 255;
+      G = G < 255 ? G : 255;
+      B = B < 255 ? B : 255;
+
+      var RR =
+        R.toString(16).length == 1 ? "0" + R.toString(16) : R.toString(16);
+      var GG =
+        G.toString(16).length == 1 ? "0" + G.toString(16) : G.toString(16);
+      var BB =
+        B.toString(16).length == 1 ? "0" + B.toString(16) : B.toString(16);
+
+      return "#" + RR + GG + BB;
     },
     wrap(d, i) {
       const selection = d3
@@ -268,6 +308,13 @@ export default {
             .attr("dy", ++lineNumber * lineHeight + dy + "em")
             .text(word);
         }
+      }
+
+      if (lineNumber > 0) {
+        const startDy = -((lineNumber - 1) * lineHeight);
+        text
+          .selectAll("tspan")
+          .attr("dy", (d, i) => startDy + lineHeight * i + "em");
       }
     }
   },
