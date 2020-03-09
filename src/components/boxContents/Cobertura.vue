90cea19ffe6f4fb4369747951878d3250c7b35fa
Marcelo Villa <marcelo.villa.p@gmail.com>
2020-03-09 10:41:10 -0500

    fix bug subtitulos

--- src/components/boxContents/Cobertura.vue
+++ src/components/boxContents/Cobertura.vue
@@ -48,7 +48,7 @@ export default {
       selectedScheme: "corine",
       subtitle: {
         corine: "clasificacionCorine",
-        proyecto: "clasificacionProyecto"
+        project: "clasificacionProyecto"
       }
     };
   },
